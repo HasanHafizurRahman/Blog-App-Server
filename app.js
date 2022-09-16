4769b1451e427d287370eadfb4b0b2e873839d12
Hasan Shanto <hasanshanto922@gmail.com>
2022-09-16 12:06:08 +0600

    crud

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 //user: HasanShanto pass: hcl0ozMdIS1clpg7
 
-const express = require('express');
-const mongoose = require('mongoose');
+import express from "express";
+import mongoose from "mongoose";
+import router from "./routes/user-routes.js";
+import blogRouter from "./routes/blog-routes";
+import cors from "cors";
 
 const app = express();
-
-
+app.use(cors());
+app.use(express.json());
+app.use("/api/user",router);
+app.use("/api/blog", blogRouter);
 
 mongoose.connect("mongodb+srv://HasanShanto:hcl0ozMdIS1clpg7@cluster0.fpeawhd.mongodb.net/?retryWrites=true&w=majority").then(() => app.listen(9000)).then(() => console.log('Connected To The MongoDB')).catch((err) => console.log(err));
 ;
--- model/User.js
+++ model/User.js
@@ -1,21 +1,22 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true,
-        minLength: 6
-    }
-})
-
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  blogs: [{ type: mongoose.Types.ObjectId, ref: "Blog", required: true }],
+});
 export default mongoose.model("User", userSchema);
